ecd9bfc8542172ca8a159dbe83b49eea99242b0a
Sergej Kukharev <sergej.kucharev@gmail.com>
2022-01-30 04:35:32 +0200

    aliases

--- db/index.js
+++ db/index.js
@@ -5,24 +5,37 @@ import Sequelize from 'sequelize';
 import configs from './config';
 
 const { env, } = process;
+const { DataTypes, } = Sequelize;
 const mode = env.NODE_ENV ?? 'development';
 const { database, username, password, ...config } = configs[mode];
 const sequelize = new Sequelize(database, username, password, config);
 
 const modelsRootPath = resolve(__dirname, 'model');
+const aliases = {
+	Game: {
+		as: 'game',
+	},
+	Player: {
+		as: 'player',
+		santa: 'santa',
+	},
+	Gift: {
+		as: 'gift',
+	},
+};
 const models = fs.readdirSync(modelsRootPath)
   .filter(file => /^[^.~!].*?[.][jt]s$/.test(file))
   .map(file => {
 		const filePath = resolve(modelsRootPath, file);
 		const Model = require(filePath);
-    const model = Model(sequelize, Sequelize.DataTypes);
+    const model = Model({ aliases, sequelize, DataTypes, });
 		return [model.name, model];
   })
 	.reduce((_, [name, model]) => Object.assign(_, {[name]: model}), {});
 
 for (const model of Object.entries(models)) {
 	const [name, instance] = model;
-	instance?.associate?.(models);
+	instance?.associate?.({ aliases, models, });
 }
 
 export default {
